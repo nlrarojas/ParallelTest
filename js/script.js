832006eb4d4f2aad029043a9779adaa60443c108
Nelson <nlrarojas@hotmail.com>
2018-06-25 14:41:10 -0600

    Implementation search of infractions made within a radius

--- js/script.js
+++ js/script.js
@@ -1,64 +1,65 @@
 var URI_API = "http://data.cityofchicago.org/resource/3v4z-xnbf.json";
+var table;
 
 $(document).ready(function() {   
-    $('#table_id').DataTable({
-        "ajax": {
-            "url": URI_API,
-            "dataSrc": ""
+  table = $('#table_id').DataTable({
+    "ajax": {
+      "url": URI_API,
+      "dataSrc": ""
+    },
+    "aoColumns": [
+      { "mData": ":@computed_region_43wa_7qmu",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": ":@computed_region_6mkv_f3dw",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": ":@computed_region_awaf_s7ux",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": ":@computed_region_bdys_3d7i",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": ":@computed_region_vrxf_vc4k",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "address",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "camera_id",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "latitude",
+        "defaultContent": "<i>Not set</i>" },            
+      { "mData": "longitude",
+        "defaultContent": "<i>Not set</i>" },
+        { "mData": "location.coordinates",
+        "render": function(data, type, full, meta){                   
+          if (data){
+            let [lng, lat] = data                  
+            return "<a id=mapLink onclick=openMap("+lng+ ',' + lat+"); data-toggle=modal data-target=#exampleModalCenter>See on map</a>";
+          } else {
+            return "<i>Not set</i>";
+          } 
+        }, 
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "violation_date",
+        "render": function(data, type, full, meta){
+          return data.split("T")[0];
         },
-        "aoColumns": [
-            { "mData": ":@computed_region_43wa_7qmu",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": ":@computed_region_6mkv_f3dw",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": ":@computed_region_awaf_s7ux",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": ":@computed_region_bdys_3d7i",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": ":@computed_region_vrxf_vc4k",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "address",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "camera_id",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "latitude",
-              "defaultContent": "<i>Not set</i>" },            
-            { "mData": "longitude",
-              "defaultContent": "<i>Not set</i>" },
-              { "mData": "location.coordinates",
-              "render": function(data, type, full, meta){                   
-                if (data){
-                  let [lng, lat] = data                  
-                  return "<a id=mapLink onclick=openMap("+lng+ ',' + lat+"); data-toggle=modal data-target=#exampleModalCenter>See on map</a>";
-                } else {
-                  return "<i>Not set</i>";
-                } 
-              }, 
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "violation_date",
-              "render": function(data, type, full, meta){
-                return data.split("T")[0];
-              },
-              "defaultContent": "<i>Not set</i>"},
-            { "mData": "violations",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "x_coordinate",
-              "defaultContent": "<i>Not set</i>" },
-            { "mData": "y_coordinate",
-              "defaultContent": "<i>Not set</i>" }
-        ],
-        "initComplete": function( settings, json ) {
-          $('#idNav').width($( document ).width());        
-          $('#idFooter').width($( document ).width());
-        }
-    });    
+        "defaultContent": "<i>Not set</i>"},
+      { "mData": "violations",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "x_coordinate",
+        "defaultContent": "<i>Not set</i>" },
+      { "mData": "y_coordinate",
+        "defaultContent": "<i>Not set</i>" }
+    ],
+    "initComplete": function( settings, json ) {
+      $('#idNav').width($( document ).width());        
+      $('#idFooter').width($( document ).width());
+    }
+  });    
 });
- 
+
 function openMap(longitude, latitude) {
   if (latitude && longitude) {    
     var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: latitude, lng: longitude},
-    zoom: 12
+      center: {lat: latitude, lng: longitude},
+      zoom: 12
     });
     infoWindow = new google.maps.InfoWindow({map: map});    
     
@@ -66,6 +67,17 @@ function openMap(longitude, latitude) {
       lat: Number(latitude),
       lng: Number(longitude)
     };    
+
+    var posMarker = {
+      lat: Number(latitude - 0.004),
+      lng: Number(longitude)
+    };
+    
+    var marker = new google.maps.Marker({
+      position: posMarker,
+      map: map,
+      title: 'Hello World!'
+    });
     
     infoWindow.setPosition(pos);
     infoWindow.setContent('Infraction made here');
---
+++ js/searchInfractions.js
@@ -0,0 +1,87 @@
+var latitude = 41.870729;
+var longitude = -87.628315;
+
+var radiusMapDistanceValue;
+
+$(document).ready(function (){
+    updateSlider(Number($('#distanceRange').val()));
+});
+
+function initSearchingMap() {    
+    var map = new google.maps.Map(document.getElementById('searchingMap'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 8
+    });
+    infoWindow = new google.maps.InfoWindow({map: map});
+    var marker = new google.maps.Marker({
+        position: {lat: latitude, lng: longitude},
+        map: map,
+        title: 'Infraction made here'
+    });
+    var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: {lat: latitude, lng: longitude}, 
+        radius: radiusMapDistanceValue * 1000
+    });
+    google.maps.event.addListener(map, 'click', function(e) {
+        marker.setPosition(e.latLng);                
+        cityCircle.setCenter(e.latLng);
+        latitude = e.latLng.lat();
+        longitude = e.latLng.lng();
+    });        
+    google.maps.event.addListener(cityCircle, 'click', function(ev) {
+        marker.setPosition(ev.latLng);                
+        cityCircle.setCenter(ev.latLng);
+        latitude = ev.latLng.lat();
+        longitude = ev.latLng.lng();
+    });
+}
+
+function updateSlider(slideAmount) {
+    radiusMapDistanceValue = Number(slideAmount);
+    initSearchingMap();
+    $('#lblDistance').text("Distance radius: " + slideAmount + " km");    
+}
+
+function executeQuery() {    
+    var distance = (radiusMapDistanceValue * 1000) / 2;
+    var JSONResult = [];
+
+    $.getJSON(URI_API, function(data){
+        
+    }).done(function (data){
+        for (var i = 0; i < Object.keys(data).length; i++) {
+            var distanceBetweenLocations = getDistance({lat: data[i].latitude, lng: data[i].longitude}, {lat: latitude, lng: longitude});
+            if (distanceBetweenLocations < distance) {
+                //console.log(distanceBetweenLocations + " Distance: " + distance);
+                JSONResult.push(data[i]);                                
+            }            
+        }                
+        table.clear();
+        table.rows.add(JSONResult);
+        table.draw();                        
+    }).fail(function (data){
+
+    });
+}
+
+var rad = function(x) {
+    return x * Math.PI / 180;
+};
+  
+var getDistance = function(p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter    
+    var dLat = rad(p2.lat - p1.lat);
+    var dLong = rad(p2.lng - p1.lng);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+};
